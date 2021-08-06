f21273124d3d6a4120ca2be22e446ce86203d4a8
Cory Piette <coryjpiette@gmail.com>
2021-08-06 13:14:23 -0400

    server.js set up

--- server.js
+++ server.js
@@ -8,6 +8,15 @@ const fs = require("fs");
 var app = express();
 var PORT = process.env.PORT || 8080
 
+//data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use("/assets", express.static("./assets"));
+
+require("./routing/html-routes")(app);
+require("./routing/api-routes")(app);
+
+// server listening
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});
